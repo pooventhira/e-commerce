c74fe42927c191ef699b32ea4d8f6d1949f5b98b
pooventhira <pooventhira9652@gmail.com>
2025-05-28 13:04:33 +0530

    Frontend folder setup

---
+++ frontend/admin-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>This is Admin App</p>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/user-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>This is User App</p>
+    </>
+  )
+}
+
+export default App
